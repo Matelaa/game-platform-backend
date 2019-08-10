c845689555be8799bf2ae142837f165fd78e28a5
José Matela <jmatelaneto@gmail.com>
2019-08-10 18:19:30 -0300

    Creating a server.js file to confing the application

---
+++ src/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const express = require('express')
+
+class App {
+  constructor() {
+    this.express = express()
+    this.isDev = process.env.NODE_ENV !== "production"
+  }
+}
+
+module.exports = new App().express
