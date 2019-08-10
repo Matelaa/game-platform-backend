f3c900205a7c9ffeb81a9fd51286338b2d746b8f
José Matela <jmatelaneto@gmail.com>
2019-08-10 19:13:16 -0300

    Creating a method to connect the database

--- src/server.js
+++ src/server.js
@@ -1,17 +1,26 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
+const databaseConfig = require('./config/database')
 
 class App {
   constructor() {
     this.express = express()
     this.isDev = process.env.NODE_ENV !== "production"
 
+    this.database()
     this.routes()
   }
 
   routes () {
     this.express.use(require('./routes'))
+  }
+
+  database() {
+    mongoose.connect(databaseConfig.uri, {
+      useNewUrlParser: true
+    })
   }
 }
 
