3b29c015eafd313739b59c44b17b31715ceeef42
José Matela <jmatelaneto@gmail.com>
2019-08-11 00:02:59 -0300

    Creating the method to create a new Player

--- src/controller/PlayerController.js
+++ src/controller/PlayerController.js
@@ -5,6 +5,21 @@ module.exports = {
     const players = await Player.find({})
 
     return res.status(200).json(players)
+  },
+
+  async store(req, res) {
+
+    const { nickname } = req.body
+
+    const playerExists = await Player.findOne({ nickname })
+
+    if (playerExists) {
+      return res.status(406).json({ error: `The player '${nickname}' already exists in our database` })
+    }
+
+    const player = await Player.create(req.body)
+
+    return res.status(201).json(player)
   }
 }
 
