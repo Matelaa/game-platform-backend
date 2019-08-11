374e175b01219d1a23bc7d3d853ac94c0794f4dc
José Matela <jmatelaneto@gmail.com>
2019-08-11 00:04:55 -0300

    Creating the Player.js to set up your scheme and communicate with the database

---
+++ src/model/Player.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const PlayerSchema = new mongoose.Schema({
+  photo: {
+    type: String,
+    required: true
+  },
+  
+  name: {
+    type: String,
+    required: true
+  },
+
+  nickname: {
+    type: String,
+    required: true
+  },
+
+  role: {
+    type: String,
+    required: true
+  },
+
+  team: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Team',
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Player', PlayerSchema)
