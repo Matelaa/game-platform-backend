e85d57bc0685d03b0c04abff16c90598893fe2ee
José Matela <jmatelaneto@gmail.com>
2019-08-11 00:06:17 -0300

    Creating the routes.js to call all application routes in a more organized way

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const PlayerController = require('./controller/PlayerController')
+const TeamController = require('./controller/TeamController')
 
 const routes = express.Router()
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello World' })
-})
+// Team
+routes.get('/teams', TeamController.index)
+routes.post('/team', TeamController.store)
+
+// Player
+routes.get('/players', PlayerController.index)
+routes.post('/player', PlayerController.store)
 
 module.exports = routes
