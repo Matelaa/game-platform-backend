83cf5c3926120872e89b3aea1e8658ea2d71beba
José Matela <jmatelaneto@gmail.com>
2019-08-11 00:01:39 -0300

    Creating the method to create a new Team

--- src/controller/TeamController.js
+++ src/controller/TeamController.js
@@ -5,5 +5,19 @@ module.exports = {
     const teams = await Team.find({})
 
     return res.status(200).json(teams)
+  },
+
+  async store(req, res) {
+    const { name } = req.body
+
+    const teamExists = await Team.findOne({ name })
+
+    if (teamExists) {
+      return res.status(406).json({ error: `The team '${name}' already exists in our database` })
+    }
+
+    const team = await Team.create(req.body)
+
+    return res.status(201).json(team)
   }
 }
