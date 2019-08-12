f4eb579ef55a37ce910889665257f5610a65b55f
José Matela <jmatelaneto@gmail.com>
2019-08-11 21:56:51 -0300

    Creating the method to delete a Player

--- src/controller/PlayerController.js
+++ src/controller/PlayerController.js
@@ -1,5 +1,5 @@
 const Player = require('../model/Player')
-
+const Team = require('../model/Team')
 module.exports = {
   async index(req, res) {
     const players = await Player.find({})
@@ -20,6 +20,23 @@ module.exports = {
     const player = await Player.create(req.body)
 
     return res.status(201).json(player)
+  },
+
+  async destroy(req, res) {
+    const { id } = req.params
+
+    const player = await Player.findById(id)
+
+    if (!player) {
+      return res.status(404).json({ error: 'This player does not exist in our database' })
+    } else if (player.team) {
+      const { name } = await Team.findById(player.team)
+      return res.status(403).json({ error: `This player is associated with the '${name}', you cannot delete someone who has a team.` })
+    }
+
+    const { name } = await Player.findByIdAndDelete(id)
+
+    res.json({ success: `The player '${name}' was successfully deleted.` })
   }
 }
 
