71f0ca1ed6407161c186f55cc7bd731b394ce5b6
José Matela <jmatelaneto@gmail.com>
2019-08-11 23:16:38 -0300

    Creating the method to delete a player from a team, and disassociate them

--- src/controller/PlayerTeamController.js
+++ src/controller/PlayerTeamController.js
@@ -30,5 +30,35 @@ module.exports = {
 
     return res.status(200).json({ player,
                                   team   })
+  },
+
+  async destroy(req, res) {
+
+    const idPlayer = req.params.idPlayer
+    const idTeam = req.params.idTeam
+
+    const player = await Player.findById(idPlayer)
+    const team = await Team.findById(idTeam)
+
+    if (!team) {
+      return res.status(404).json({ error: 'Team not exists.' })
+    } else if (!player) {
+      return res.status(404).json({ error: 'Player not exists.' })
+    }
+
+    if (!player.team) {
+      return res.status(404).json({ error: `This player don't have a team` })
+    }
+
+    player.team = null
+
+    const index = team.players.indexOf(idPlayer)
+    team.players.splice(index, 1)
+
+    await player.save()
+    await team.save()
+    
+    return res.status(200).json({ player,
+                                  team   })
   }
 }
