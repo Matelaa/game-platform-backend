d676d07d478e752c1fa91e7ba6681af23bfbfe8c
José Matela <jmatelaneto@gmail.com>
2019-08-14 03:01:51 -0300

    Refactoring the create and delete method if the player has a team

--- src/controller/PlayerController.js
+++ src/controller/PlayerController.js
@@ -10,16 +10,43 @@ module.exports = {
   async store(req, res) {
 
     const { nickname } = req.body
+    const { team } = req.body
 
     const playerExists = await Player.findOne({ nickname })
+    const teamExists = await Team.findOne({ name: team })
 
     if (playerExists) {
+
       return res.status(406).json({ error: `The player '${nickname}' already exists in our database` })
+
+    } else if (team === undefined) {
+
+      const player = await Player.create(req.body)
+      return res.status(201).json(player)
+
+    } else if (team !== null) {
+
+      if (teamExists) {
+        
+        const team = await Team.findById(teamExists._id)
+        const player = await Player.create({ photo: req.body.photo,
+                                             name: req.body.name,
+                                             nickname: req.body.nickname,
+                                             role: req.body.role,
+                                             team: team })
+
+        player.team = team._id
+
+        team.players.push(player._id)
+        await team.save()
+
+        return res.status(201).json({ player,
+                                      team })
+                                      
+      } else {
+        return res.status(400).json({ error: `The team '${team}' doesnt exists.` })
+      }
     }
-
-    const player = await Player.create(req.body)
-
-    return res.status(201).json(player)
   },
 
   async destroy(req, res) {
@@ -29,14 +56,25 @@ module.exports = {
 
     if (!player) {
       return res.status(404).json({ error: 'This player does not exist in our database' })
+
     } else if (player.team) {
-      const { name } = await Team.findById(player.team)
-      return res.status(403).json({ error: `This player is associated with the '${name}' team, you cannot delete someone who has a team.` })
+
+      const team = await Team.findById(player.team)
+
+      player.team = null
+
+      const index = team.players.indexOf(id)
+      team.players.splice(index, 1)
+
+      await Player.findByIdAndDelete(id)
+      await team.save()
+
+      return res.status(200).json({ success: `The player '${player.name}' was successfully deleted.` })
     }
 
     const { name } = await Player.findByIdAndDelete(id)
 
-    res.status(200).json({ success: `The player '${name}' was successfully deleted.` })
+    return res.status(200).json({ success: `The player '${name}' was successfully deleted.` })
   }
 }
 
